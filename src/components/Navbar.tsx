fcfbf8bd8fa278bd2515ccde91ea1b0efdf11319
Matacos3 <acosta.garcia.mateo@gmail.com>
2024-04-22 17:30:16 +0200

    added new page, and modal

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -75,7 +75,6 @@ export default function Navbar(props) {
           )}
         </div>
       </nav>
-      
     </>
   );
 }
--- src/pages/fe/feOnDb.tsx
+++ src/pages/fe/feOnDb.tsx
@@ -1,10 +1,45 @@
 import Navbar from "../../components/Navbar";
+import { useState } from "react";
+export default function feOnDbPage() {
+  //setting states
+  //showing modal or not
+  const [showModal, setShowModal] = useState(false);
 
-export default function feOnDbPage(){
-  return(
-    <div>
-      <Navbar />
-      fe on db
+  //handling adding a FE in DB
+
+  const addFe = () => {
+    console.log("we are adding FE");
+  };
+
+  //coding modal
+
+  const modal = (
+    <div className="absolute top-1/2 left-1/2 bg-color1 h-52 w-96 -translate-x-1/2 -translate-y-1/2 rounded text-color2 flex flex-col justify-center items-center">
+      <div
+        onClick={() => setShowModal(false)}
+        className=" p-2 cursor-pointer text-color2 absolute top-0 right-0 hover:text-color3"
+      >
+        Close
+      </div>
+      <h3 className="text-color2">Ajoutez un fe ici</h3>
+      <form></form>
     </div>
-  )
+  );
+
+  return (
+    <div className="h-screen relative ">
+      <Navbar />
+      {showModal && modal}
+      <div className="flex items-center justify-center flex-col">
+        <h1>Base de données facteurs d’émissions</h1>
+        <button
+          onClick={() => setShowModal(true)}
+          className="bg-color1 text-color3 p-2 rounded transition-all hover:bg-color2"
+        >
+          Ajoutez un FE dans la base de données
+        </button>
+        <div className="bg-color1 w-4/5"></div>
+      </div>
+    </div>
+  );
 }
